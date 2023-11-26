524c06a0514c09e87246d25177c25786558c90bb
Hasan <hasankoura8@gmail.com>
2023-11-26 19:24:15 +0200

    Change port

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3000;
+const port = 10000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
